c88f1bb227f42503f63f53ca8a6a832eae18b679
mp1707 <marco.preuss1707@gmail.com>
2023-08-29 22:03:07 +0200

    wip

--- src/routes/ChatRPG.tsx
+++ src/routes/ChatRPG.tsx
@@ -34,47 +34,73 @@ const ChatRPG = () => {
 
   return (
     <LayoutContainer>
-      <form onSubmit={onSubmit} className="form-control w-full max-w-xs gap-3">
-        <div>
-          <label className="label label-text">describe the gameworld</label>
-          <textarea
-            placeholder="Type here"
-            className="textarea textarea-bordered w-full max-w-xs"
-            value={rpgData.world}
-            onChange={(e) => setRpgData({ ...rpgData, world: e.target.value })}
-          />
-        </div>
-        <div>
-          <label className="label label-text">describe the scenario</label>
-          <textarea
-            placeholder="Type here"
-            className="textarea textarea-bordered w-full max-w-xs"
-            value={rpgData.scenario}
-            onChange={(e) =>
-              setRpgData({ ...rpgData, scenario: e.target.value })
-            }
-          />
-        </div>
-        <div>
-          <label className="label label-text">add player</label>
-          <div className="flex gap-2">
-            <input
-              type="text"
-              placeholder="player name"
-              className="input input-bordered w-full max-w-xs"
-              onChange={(e) => setNewPlayerName(e.target.value)}
-              value={newPlayerName}
+      <div className="flex flex-col gap-3 items-center">
+        <span className="text-3xl font-bold">chatRPG</span>
+        <form
+          onSubmit={onSubmit}
+          className="form-control w-full max-w-xs gap-2"
+        >
+          <div>
+            <label className="label label-text">describe the gameworld</label>
+            <textarea
+              placeholder="Type here"
+              className="textarea textarea-bordered w-full max-w-xs"
+              value={rpgData.world}
+              onChange={(e) =>
+                setRpgData({ ...rpgData, world: e.target.value })
+              }
             />
-            <button onClick={handleNewPlayer} className="btn btn-neutral">
-              +
-            </button>
           </div>
-          {rpgData.players?.map((player) => {
-            return <div> {player.name}</div>;
-          })}
-        </div>
-        <input type="submit" value="submit" className="btn" />
-      </form>
+          <div>
+            <label className="label label-text">describe the scenario</label>
+            <textarea
+              placeholder="Type here"
+              className="textarea textarea-bordered w-full max-w-xs"
+              value={rpgData.scenario}
+              onChange={(e) =>
+                setRpgData({ ...rpgData, scenario: e.target.value })
+              }
+            />
+          </div>
+          <div>
+            <label className="label label-text">add player</label>
+            <div className="flex flex-col gap-2">
+              <div className="flex gap-2">
+                <input
+                  type="text"
+                  placeholder="player name"
+                  className="input input-bordered w-full max-w-xs"
+                  onChange={(e) => setNewPlayerName(e.target.value)}
+                  value={newPlayerName}
+                />
+                <button onClick={handleNewPlayer} className="btn btn-neutral">
+                  +
+                </button>
+              </div>
+              <div className="flex flex-wrap gap-2">
+                {rpgData.players?.map((player) => {
+                  return (
+                    <button
+                      className="badge badge-neutral"
+                      onClick={() => {
+                        setRpgData({
+                          ...rpgData,
+                          players: rpgData.players?.filter(
+                            (item) => item.name !== player.name
+                          ),
+                        });
+                      }}
+                    >
+                      {player.name}
+                    </button>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+          <input type="submit" value="start game" className="btn" />
+        </form>
+      </div>
     </LayoutContainer>
   );
 };
